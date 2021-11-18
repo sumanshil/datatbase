8f9d26496687f52b55361cf6c7ee607bd87d1d5a
Suman Chandra Shil <sshil@pinterest.com>
2021-11-17 18:15:30 -0800

    initial commit

---
+++ cmake-build-debug/CMakeFiles/DB_C.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DB_C.dir/main.c.o"
+  "DB_C"
+  "DB_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DB_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
